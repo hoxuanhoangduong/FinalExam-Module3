cd04c604f7003e0ab5199e647021da9580fdc0e8
Ho Duong <hxhduong4394@gmail.com>
2019-11-05 09:33:43 +0700

    Add interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookUpdateComponent } from './book-update/book-update.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookCreateComponent,
+    BookUpdateComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule
