875172a456aaac0f9866735c0d410de394dd1080
Ho Duong <hxhduong4394@gmail.com>
2019-11-05 10:31:26 +0700

    Add Book-create

--- src/app/book-create/book-create.component.ts
+++ src/app/book-create/book-create.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {BookService} from '../book.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-create',
@@ -7,10 +10,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BookCreateComponent implements OnInit {
 
-  constructor() {
+  data: FormGroup;
+
+  constructor(private fb: FormBuilder, private bookDervice: BookService, private router: Router) {
   }
 
   ngOnInit() {
+    this.data = this.fb.group({
+        id: '',
+        title: '',
+        author: '',
+        description: ''
+      }
+    );
   }
 
+  createBook() {
+    this.bookDervice.createBook(this.data.value).subscribe(next => {
+      this.router.navigate(['/home']);
+    });
+    console.log(this.data.value);
+  }
 }
