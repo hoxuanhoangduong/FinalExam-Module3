cc43e10c08822f1277add1be5cb9a4314447d63c
Ho Duong <hxhduong4394@gmail.com>
2019-11-05 10:39:15 +0700

    Add delete & update

--- src/app/book-update/book-update.component.ts
+++ src/app/book-update/book-update.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {IBook} from '../book.interface';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BookService} from '../book.service';
 
 @Component({
   selector: 'app-book-update',
@@ -6,11 +10,39 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./book-update.component.css']
 })
 export class BookUpdateComponent implements OnInit {
+  book: IBook;
+  data: FormGroup;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private bookService: BookService,
+              private router: Router,
+              private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    this.data = this.fb.group({
+      id: '',
+      title: '',
+      author: '',
+      description: ''
+    })
+    ;
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.bookService.getById(id).subscribe(
+      next => {
+        this.book = next;
+        this.data.patchValue(this.book);
+      },
+      error => {
+        console.log(error);
+        this.book = null;
+      }
+    );
   }
 
+  editBook() {
+    this.bookService.updateBook(this.data.value).subscribe(next => {
+      this.router.navigate(['/home']);
+    });
+  }
 }
